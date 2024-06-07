816ef25383f21a882148d0eb5fe29035f4e23266
Ximpler <apmichelle@uninorte.edu.co>
2024-06-06 19:24:46 -0500

    Grades in edit list

--- src/components/StudentTable.jsx
+++ src/components/StudentTable.jsx
@@ -1,37 +1,47 @@
-import { Flex, Button } from '@chakra-ui/react'
-import { TableContainer, Table, Thead, Tbody, Td, Tr, Th } from '@chakra-ui/react'
+import { Flex, Button } from '@chakra-ui/react';
+import { TableContainer, Table, Thead, Tbody, Td, Tr, Th } from '@chakra-ui/react';
 import { Link } from 'react-router-dom';
 import Swal from 'sweetalert2';
 
-
 export default function StudentTable(props) {
-return (
+  return (
     <TableContainer>
-        <Table variant='simple'>
-            <Thead>
-                <Tr>
-                    <Th>Name</Th>
-                    <Th>Edad</Th>
-                    <Th>Action</Th>
-                </Tr>
-            </Thead>
-            <Tbody>
-                {props.data.map((student, i) => (
-                   <Tr key={i}>
-                   <Td>{student.name}</Td>
-                   <Td>{student.age}</Td>
-                   <Td>
-                       <Flex>
-                           <Link to={"/editStudent/" + student.id}>
-                               <Button mr={2}>Edit</Button>
-                           </Link>
-                       </Flex>
-                   </Td>
-               </Tr>
-                ))}
-            </Tbody>
-        </Table>
+      <Table variant='simple'>
+        <Thead>
+          <Tr>
+            <Th>Name</Th>
+            <Th>Age</Th>
+            <Th>Grades</Th>
+            <Th>Action</Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          {props.data.map((student, i) => (
+            <Tr key={i}>
+              <Td>{student.name}</Td>
+              <Td>{student.age}</Td>
+              <Td>
+                {student.grades.length > 0 ? (
+                  <ul>
+                    {student.grades.map((grade, index) => (
+                      <li key={index}>Semester {grade.semester}: {grade.grade}</li>
+                    ))}
+                  </ul>
+                ) : (
+                  <span>No grades available</span>
+                )}
+              </Td>
+              <Td>
+                <Flex>
+                  <Link to={"/editStudent/" + student._id}>
+                    <Button mr={2}>Edit</Button>
+                  </Link>
+                </Flex>
+              </Td>
+            </Tr>
+          ))}
+        </Tbody>
+      </Table>
     </TableContainer>
-)
+  );
 }
-
